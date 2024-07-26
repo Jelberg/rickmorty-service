b3770c4fe28a304a14fe1f067230ff8b879009a8
Jessica Elberg <thejessidev@gmail.com>
2024-07-26 11:34:05 -0400

    change status to suspended character

--- src/characters/services/characters.service.ts
+++ src/characters/services/characters.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/services/prisma.service';
 import { Prisma, characters as CharactersModel } from '@prisma/client';
+import { STATUS } from 'src/commons/enum';
 
 @Injectable()
 export class CharactersService {
@@ -24,7 +25,25 @@ export class CharactersService {
     return `This action returns a #${id} episode`;
   }
 
-  async remove(id: number) {
-    return `This action removes a #${id} episode`;
+  async deleteCharacter(params: { where: Prisma.episodesWhereUniqueInput }) {
+    try {
+      const { where } = params;
+
+      const typeStatus = await this.prisma.status.findFirst({
+        include: {
+          type_stat: true,
+        },
+        where: { name: STATUS.SUSPENDED },
+      });
+
+      return this.prisma.episodes.update({
+        where,
+        data: {
+          fk_typestat: typeStatus.type_stat[0].id,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
