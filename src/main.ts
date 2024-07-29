085cb7ca0859f2c036677a46c24e125791be21d7
Jessica Elberg <thejessidev@gmail.com>
2024-07-28 23:24:59 -0400

    add port

--- src/main.ts
+++ src/main.ts
@@ -31,7 +31,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  console.log('Server is running on port 3000!');
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  console.log(`Server is running on port ${port}!`);
+  await app.listen(port);
 }
 bootstrap();
